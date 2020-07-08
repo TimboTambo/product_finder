119e87c16b21eff44d13847c337813e81963c06b
Tim Lee <tim.lee@red-badger.com>
2020-07-08 15:12:54 +0100

    Add mocha and tests dir

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ let itemNames = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"]
 
 // TODO: add validation
 
-// Take user input
-// <Possibly validate>
-// Check list to see if it contains user input value
-// Return true or false
-
 function inputHandler(answer) {
   let result = itemNames.includes(answer)
   if (result) {
