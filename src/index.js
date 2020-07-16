da02db35dd344b5dae18487dfab0ba393803727b
Tim Lee <tim.lee@red-badger.com>
2020-07-16 14:02:49 +0100

    Switch to searching product object

--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ const product = require("./models/product.js")
 let rl = readline.createInterface(process.stdin, process.stdout)
 
 let itemNames = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"]
+product1 = new product("PL123456", "name", "dose", "substance", "url")
+product2 = new product("PL513872", "name", "dose", "substance", "url")
+product3 = new product("PL999999", "name", "dose", "substance", "url")
+product4 = new product("PL654321", "name", "dose", "substance", "url")
+product5 = new product("PL818181", "name", "dose", "substance", "url")
+let products = [product1, product2, product3, product4, product5]
 
-function isProductPresent(product) {
-  return itemNames.includes(product)
+let productObjects = {}
+for (let i = 0; i < products.length; i++) {
+  let product = products[i]
+  productObjects[product.plNumber] = product
+}
+
+// productObjects[""]
+
+function getProduct(product) {
+  return productObjects[product]
 }
 
 function inputHandler(answer) {
-  let result = isProductPresent(answer)
+  let result = getProduct(answer)
   if (result) {
     console.log("Product exists", answer)
   } else {
---
+++ src/models/product.test.js
@@ -0,0 +1,21 @@
+const product = require("./product.js")
+
+describe("Product class test", () => {
+  test("can be created", () => {
+    let product1 = new product("plNumber", "name", "dose", "substance", "url")
+    expect(product1.plNumber).toBe("plNumber")
+  }),
+    test("can be created", () => {
+      console.log = jest.fn()
+      let product1 = new product("plNumber", "name", "dose", "substance", "url")
+      let expectedOutput = `
+    PL Number: plNumber
+    Name: name
+    Dose: dose
+    Substance: substance
+    URL: url
+    `
+      product1.description()
+      expect(console.log.mock.calls[0][0]).toBe(expectedOutput)
+    })
+})
